d483a2c98ec859425d77fdceeaa09ed368461bc2
h74.amirLitan <amirlitan@gmail.com>
2023-08-27 10:03:01 +0300

    updata main

--- tests/client/main.cpp
+++ tests/client/main.cpp
@@ -1,16 +1,22 @@
 #include <QCoreApplication>
 #include <QDebug>
+#include <iostream>
 #include "client.hpp"
+#include "ui.hpp"
+
+#include <QApplication>
+#include <QPushButton>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    QApplication app(argc, argv);
 
     qDebug() << "Starting Client...";
 
     spd::Client client;  // Client object is instantiated, and its manager is automatically constructed.
     
     client.connect_to_server("127.0.0.1", 5555);
+    client.run_app();
 
-    return a.exec(); // Let the application run, waiting for events/data from the server.
+    return app.exec(); // Let the application run
 }
