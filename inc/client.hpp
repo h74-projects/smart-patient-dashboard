557a13c351cc321aff932eb6307d7a23f40484ab
h74.amirLitan <amirlitan@gmail.com>
2023-08-27 10:02:21 +0300

    add func handl ui

--- inc/client.hpp
+++ inc/client.hpp
@@ -4,6 +4,8 @@
 #include <QString>
 #include <memory>
 #include "client_manager.hpp"
+#include "ui.hpp"
+#include "event.hpp"
 
 namespace spd {
 class Client {
@@ -11,9 +13,14 @@ public:
     Client();
 
     void connect_to_server(const QString &address, quint16 port);
-
+    void run_app();
+    
+private:
+    void receive_event();
 private:
     ClientManager m_manager;
+    UI m_ui;
+    bool m_is_eactive;
     //List User; <-id,name,password
 };
 
--- src/client.cpp
+++ src/client.cpp
@@ -1,18 +1,37 @@
 #include "client.hpp"
+#include "event.hpp"
+#include "client_manager.hpp"
+#include <iostream>
 
 namespace spd {
 
 Client::Client()
+: m_is_eactive{false}
 {
 }
 
 void Client::connect_to_server(const QString &address, quint16 port) 
 {
+
     m_manager.connect_to_server(address, port);
 }
 
-// connect(&clientManagerInstance, &ClientManager::newDataReceived, this, [&](QDateTime timeStamp, QString eventType, QString eventData, QString eventLocation) {
-//     // Handle the data in the Client, e.g., update the UI.
-// });
-
+void Client::run_app()
+{
+    //ask client manager if event is active
+    m_is_eactive = m_manager.is_event_active();
+    receive_event();
 }
+
+void Client::receive_event()
+{
+    while(m_is_eactive) {
+        m_ui.show_event(m_manager.current_event().getTimestamp()
+                        , m_manager.current_event().getEventType()
+                        , m_manager.current_event().getEventData()
+                        , m_manager.current_event().getEventLocation());
+        m_is_eactive = false;
+    }
+}
+
+} // namespace spd
